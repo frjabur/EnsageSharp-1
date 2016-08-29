443158cd9b8b0f8eaec67fcccda73acfd45b722a
osmanbonus007 <osmanbonus007@mail.ru>
2016-08-29 23:09:11 +0300

    Update BuildInfo.cs

--- Ensage.Resource/BuildInfo.cs
+++ Ensage.Resource/BuildInfo.cs
@@ -1,2 +1,2 @@
-public class BuildInfo { public const string ProjectDir = @"C:\Users\osman\Desktop\Ensage-2-master\Ensage.Resource\"; public const string BuildStamp = @"08/29/2016 22:52:26"; } 
+public class BuildInfo { public const string ProjectDir = @""; public const string BuildStamp = @"08/29/2016 22:52:26"; } 
 
