6f91cef48040443bce207d63258de33d6782b929
osmanbonus007 <osmanbonus007@mail.ru>
2017-04-11 11:34:23 +0300

    update

---
+++ ZoomParticleHack/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZoomParticleHack")]
+[assembly: AssemblyProduct("ZoomParticleHack")]
+[assembly: AssemblyCopyright("Copyright © dendimonster 2016")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0")]
---
+++ ZoomParticleHack/Program.cs
@@ -0,0 +1,85 @@
+using System;
+using Ensage;
+using Ensage.Common.Menu;
+
+namespace ZoomParticleHack
+{
+	class Program
+	{
+		
+		private static readonly Menu Menu = new Menu("Zoom Particle Hack", "Zoom Particle Hack", true);
+		private static readonly uint VK_CTRL = 0x11;
+		private static readonly uint WM_MOUSEWHEEL = 0x020A;
+		private static readonly ConVar ZoomVar = Game.GetConsoleVar("dota_camera_distance");
+		private static readonly ConVar renderVar = Game.GetConsoleVar("r_farz");
+		static void Main()
+		{
+			Game.OnWndProc += Game_OnWndProc;
+			Game.OnUpdate += Game_OnUpdate;
+		}
+		private static bool loaded;
+		private static void Game_OnUpdate(EventArgs args)
+		{
+			
+			if (!Game.IsInGame)
+			{
+				loaded = false;
+				return;
+			}
+			if (loaded)
+			{
+				return;
+			}
+			// <HackParticle>
+			Game.GetConsoleVar("fog_enable").SetValue(0);
+			Game.GetConsoleVar("fow_client_nofiltering").SetValue(1);
+			Game.GetConsoleVar("dota_use_particle_fow").SetValue(0);
+	        	Game.GetConsoleVar("dota_use_particle_fow_unbloated").SetValue(0);
+			
+			var player = ObjectManager.LocalPlayer;
+			if ((player == null) || (player.Team == Team.Observer))
+				return;
+			Game.PrintMessage("<font color='#aaaaaa'>Zoom Particle Hack </font><font color='#33ff66'>Loaded</font>");
+			var slider = new MenuItem("distance", "Camera Distance").SetValue(new Slider(1550, 1134, 3100));
+			slider.ValueChanged += Slider_ValueChanged;
+			Menu.AddItem(slider);
+			Menu.AddToMainMenu();
+			ZoomVar.RemoveFlags(ConVarFlags.Cheat);
+			renderVar.RemoveFlags(ConVarFlags.Cheat);
+			ZoomVar.SetValue(slider.GetValue<Slider>().Value);
+			renderVar.SetValue(2*(slider.GetValue<Slider>().Value));
+			loaded = true;
+		}
+		private static void Slider_ValueChanged(object sender, OnValueChangeEventArgs e)
+		{
+			var player = ObjectManager.LocalPlayer;
+			if ((player == null) || (player.Team == Team.Observer))
+				return;
+				ZoomVar.SetValue(e.GetNewValue<Slider>().Value);
+				renderVar.SetValue(2*(e.GetNewValue<Slider>().Value));
+		}
+		private static void Game_OnWndProc(WndEventArgs args)
+		{
+			if (args.Msg == WM_MOUSEWHEEL && Game.IsInGame )
+			{
+				var player = ObjectManager.LocalPlayer;
+				if ((player == null) || (player.Team == Team.Observer))
+					return;
+				if (Game.IsKeyDown(VK_CTRL))
+				{
+					var delta = (short)((args.WParam >> 16) & 0xFFFF);
+					var zoomValue = ZoomVar.GetInt();
+					if (delta < 0)
+						zoomValue += 50;
+					if (delta > 0) 
+						zoomValue -= 50;
+					if (zoomValue < 1134)
+						zoomValue = 1134;
+					ZoomVar.SetValue(zoomValue);
+					Menu.Item("distance").SetValue(new Slider(zoomValue, 1134, 3100));
+					args.Process = false;
+				}	
+			}
+		}
+	}
+}
